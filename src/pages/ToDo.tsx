9e280b07f4a420a41b05b35f6f768796ff7f8a05
Yjason-K <gomgom0324@gmail.com>
2023-09-15 00:48:08 +0900

    fix: addHandler 예외처리

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import CustomButton from "../components/Button";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { Todo } from "../lib/types/todo";
 import "../styles/pages/Todo.css";
@@ -8,45 +8,6 @@ import "../styles/pages/Todo.css";
 import ToDolists from "../components/ToDoLists";
 
 const ToDo = () => {
-  // logoutHandler
-  const navigate = useNavigate();
-  const logoutHandler = () => {
-    localStorage.removeItem("token");
-    navigate("/login", { replace: true });
-  };
-
-  // addHandler
-  const addHandler = async () => {
-    const token = localStorage.getItem("token");
-    const parsedToken = token ? JSON.parse(token) : null;
-    await axios({
-      method: "post",
-      url: `${process.env.REACT_APP_WANTED_API}/todos`,
-      headers: {
-        Authorization: `Bearer ${parsedToken}`,
-        "Content-Type": "application/json",
-      },
-      data: {
-        todo: nwetodo,
-      },
-    }).then(() => {
-      axios({
-        method: "get",
-        url: `${process.env.REACT_APP_WANTED_API}/todos`,
-        headers: {
-          Authorization: `Bearer ${parsedToken}`,
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => {
-          setTodoLists(res.data);
-        })
-        .catch((err) => {
-          window.alert("할일을 불러오지 못하였습니다!!");
-        });
-    });
-  };
-
   // todoLists
   const [todoLists, setTodoLists] = useState<Todo[]>([]);
   useEffect(() => {
@@ -77,6 +38,54 @@ const ToDo = () => {
     setNewTodo(e.target.value);
   };
 
+  // logoutHandler
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    navigate("/login", { replace: true });
+  };
+
+  // addHandler
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const addHandler = async () => {
+    if (nwetodo !== "") {
+      const token = localStorage.getItem("token");
+      const parsedToken = token ? JSON.parse(token) : null;
+      await axios({
+        method: "post",
+        url: `${process.env.REACT_APP_WANTED_API}/todos`,
+        headers: {
+          Authorization: `Bearer ${parsedToken}`,
+          "Content-Type": "application/json",
+        },
+        data: {
+          todo: nwetodo,
+        },
+      }).then(() => {
+        axios({
+          method: "get",
+          url: `${process.env.REACT_APP_WANTED_API}/todos`,
+          headers: {
+            Authorization: `Bearer ${parsedToken}`,
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => {
+            setTodoLists(res.data);
+          })
+          .catch((err) => {
+            window.alert("할일을 불러오지 못하였습니다!!");
+          });
+      });
+    } else {
+      window.alert("할일을 입력해주세요.");
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }
+  };
+
   return (
     <div className="todo">
       <CustomButton
@@ -86,14 +95,16 @@ const ToDo = () => {
         testid=""
         handler={logoutHandler}
       />
+      <h2>새로운 Todo 작성하기 ✏️</h2>
       <div className="addtodo">
-        <h2>새로운 Todo 작성하기 ✏️</h2>
         <input
           type="text"
           data-testid="new-todo-input"
           className="add_input"
           value={nwetodo}
+          placeholder="할일을 작성해주세요."
           onChange={newtodoChange}
+          ref={inputRef}
         />
         <CustomButton
           type="add"
